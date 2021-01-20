e462396539420866db5df56b43a1e634344e8ee0
Ben Micol <benmicol@gmail.com>
2021-01-20 17:09:00 -0500

    added messages to chatbot

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,21 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import {connect} from "react-redux"
 
-const Chat = () => {
+import {userMessage, sendMessage} from "../../actions/watson"
+
+const Chat = ({chat, userMessage, sendMessage}) => {
     const [message, setMessage] = useState("")
+    const endOfMessages = useRef(null)
+    const scrollToBottom = () =>{
+        endOfMessages.current.scrollIntoView({behavior: "smooth"})
+    }
+    useEffect(scrollToBottom,[chat])
     const handleClick = async (e) =>{
         const code = e.keyCode || e.which
         if (code === 13) {
             console.log(message)
+            userMessage(message)
+            sendMessage(message)
             setMessage("")
         }
     }
     return (
         <div className="chat">
             <h1>Chatty the chatbot</h1>
-            <div>Messages go here</div>
+            <div className="historyContainer">
+            {chat.length===0 ? "" : chat.map(msg=><div className={msg.type}>{msg.message}</div>)}
+            <div ref={endOfMessages}></div>
+            </div>
             <input id="chatbox" onChange={(e)=>setMessage(e.target.value)} onKeyPress={handleClick} value={message}></input>
         </div>
     )
 }
+const mapStateToProps = (state) =>({
+    chat: state.watson.messages,
+})
 
-export default Chat
+export default connect( mapStateToProps, {userMessage, sendMessage})(Chat)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,5 @@
 import {combineReducers} from "redux"
 
-export default combineReducers({})
+import watson from "./watson"
+
+export default combineReducers({watson})
